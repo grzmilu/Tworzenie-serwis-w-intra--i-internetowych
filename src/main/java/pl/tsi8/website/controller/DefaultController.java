6c5e017e33d471924fbfcbd482cc6e022b965388
Łukasz Pruski <lukasz_pruski@outlook.com>
2018-01-07 00:54:26 +0100

    spring boot project setup

---
+++ src/main/java/pl/tsi8/website/controller/DefaultController.java
@@ -0,0 +1,28 @@
+package pl.tsi8.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "/home";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "/user";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
+
+	@GetMapping("/403")
+	public String error403() {
+		return "/error/403";
+	}
+}
